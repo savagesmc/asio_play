968b71ac5756ea339f6a031bedb6dcf6a0798c05
Clark, Steve <steve@clarkhome.org>
2020-02-16 14:54:14 -0600

    Adds global mutex to protect std::cout.

--- asio_thread_pool_example.cpp
+++ asio_thread_pool_example.cpp
@@ -4,21 +4,32 @@
 #include <thread>
 #include <vector>
 #include <boost/asio.hpp>
+#include <mutex>
 
 using namespace std;
 namespace ba = boost::asio;
 
+mutex global_stream_lock;
+
 void doWork(shared_ptr<ba::io_context> io_context) {
-   std::cout << "Thread Start\n";
+   global_stream_lock.lock();
+   std::cout << "[" << this_thread::get_id() << "] Thread Start" << endl;
+   global_stream_lock.unlock();
+
    io_context->run();
-   std::cout << "Thread Finish\n";
+
+   global_stream_lock.lock();
+   std::cout << "[" << this_thread::get_id() << "] Thread Stop" << endl;
+   global_stream_lock.unlock();
 }
 
 int main( int argc, char * argv[] ) {
    auto io_context = make_shared<ba::io_context>();
    auto work = make_shared<ba::io_context::work>(*io_context);
 
+   global_stream_lock.lock();
    std::cout << "Press [return] to exit." << std::endl;
+   global_stream_lock.unlock();
 
    vector<thread> threadPool;
    for( int x = 0; x < 4; ++x ) {
